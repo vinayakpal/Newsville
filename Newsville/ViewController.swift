5f0e94865328508906caecd3a4593d6af8ea136d
Vinayak Pal <vinayak@so.city>
2019-04-03 02:54:36 +0530

    base project add

---
+++ Newsville/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newsville
+//
+//  Created by Vinayak Pal on 03/04/19.
+//  Copyright © 2019 Vinayak Pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
